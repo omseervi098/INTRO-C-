84ff54f27cf41608cada1a00c5196c3da71c9d31
Omprakash Choudhary <omseervi098@gmail.com>
2022-01-13 15:31:12 +0530

    added 2nd largest element

---
+++ Searching Sorting/2ndlargestelem.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findSecondLargest(int *input, int n)
+{
+    int max1=input[0],max2=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(input[i]>max1)
+        {
+            max2=max1;
+            max1=input[i];
+        }else if(input[i]>max2 && input[i]!=max1)
+        {
+            max2=input[i];
+        }
+    }
+    return max2;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			cin >> input[i];
+		}
+		cout << findSecondLargest(input, size) << endl;
+		delete[] input;
+	}
+	return 0;
+}
