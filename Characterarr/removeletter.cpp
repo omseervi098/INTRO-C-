f894af02d6299401e14e4666555edb9233f1f3d4
Omprakash Choudhary <omseervi098@gmail.com>
2022-01-25 08:05:13 +0530

    Create removeletter.cpp

---
+++ Characterarr/removeletter.cpp
@@ -0,0 +1,28 @@
+
+/*
+input=aabbccaad
+a
+output=bbccd
+*/#include <iostream>
+#include <cstring>
+using namespace std;
+void removeAllOccurrencesOfChar(char input[], char c) {
+    int j=0;
+    for(int i=0;input[i]!='\0';i++)
+    {
+        if(input[i]!=c)
+        {
+            input[j]=input[i];
+            j++;
+        }
+    }input[j]='\0';
+}
+int main() {
+    int size = 1e6;
+    char str[size];
+    cin.getline(str, size);
+    char c;
+    cin >> c;
+    removeAllOccurrencesOfChar(str, c);
+    cout << str;
+}
